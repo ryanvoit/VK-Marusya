8107b973b34229adc2653dff89e1c8c2e4533758
ryanvoit <ryanvoit@yandex.ru>
2026-07-03 18:30:03 +0300

    styles scss fixed

--- src/components/Main/GenrePage/GenrePageComponent/GenrePageComponent.tsx
+++ src/components/Main/GenrePage/GenrePageComponent/GenrePageComponent.tsx
@@ -15,7 +15,6 @@ export const GenrePageComponent: FC<GenrePageProps> = ({ movies, genre }) => {
                 <Image src='/back.svg' alt="back" width={40} height={40}></Image>
                 <h1 className="genre-page__title">{genre[0].toUpperCase() + genre.slice(1)}</h1>
             </Link>
-
             <ul className="genre-page__list">
                 {movies.map((movie) => (
                     <li className="genre-page__item" key={movie.id}>
